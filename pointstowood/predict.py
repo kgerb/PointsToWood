34c6906f287461484f1f0c0ae066ec342827eda2
Kilian Gerberding <ki-facebook@freenet.de>
2025-09-16 13:27:03 +0200

    added possibility to choose which gpu to process on

--- pointstowood/predict.py
+++ pointstowood/predict.py
@@ -1,4 +1,5 @@
 import datetime
+
 start = datetime.datetime.now()
 import resource
 import os
@@ -14,66 +15,141 @@ import numpy as np
 import re
 from src.io import load_file, save_file
 
+
 def set_num_threads(num_threads):
     torch.set_num_threads(num_threads)
-    os.environ['OMP_NUM_THREADS'] = str(num_threads)
+    os.environ["OMP_NUM_THREADS"] = str(num_threads)
 
-'''
+
+"""
 Minor functions-------------------------------------------------------------------------------------------------------------
-'''
+"""
+
 
 def get_path(location_in_pointstowood: str = "") -> str:
     current_wdir = os.getcwd()
-    match = re.search(r'PointsToWood.*?pointstowood', current_wdir, re.IGNORECASE)
+    match = re.search(r"PointsToWood.*?pointstowood", current_wdir, re.IGNORECASE)
     if not match:
-        raise ValueError('"PointsToWood/pointstowood" not found in the current working directory path')
+        raise ValueError(
+            '"PointsToWood/pointstowood" not found in the current working directory path'
+        )
     last_index = match.end()
     output_path = current_wdir[:last_index]
     if location_in_pointstowood:
         output_path = os.path.join(output_path, location_in_pointstowood)
     return output_path.replace("\\", "/")
 
+
 def preprocess_point_cloud_data(df):
     df.columns = df.columns.str.lower()
-    columns_to_drop = ['label', 'pwood', 'pleaf']
-    df = df.drop(columns=columns_to_drop, errors='ignore')
-    df = df.rename(columns=lambda x: x.replace('scalar_', '') if 'scalar_' in x else x)
-    df = df.rename(columns={'refl': 'reflectance', 'intensity': 'reflectance'})
+    columns_to_drop = ["label", "pwood", "pleaf"]
+    df = df.drop(columns=columns_to_drop, errors="ignore")
+    df = df.rename(columns=lambda x: x.replace("scalar_", "") if "scalar_" in x else x)
+    df = df.rename(columns={"refl": "reflectance", "intensity": "reflectance"})
     headers = [header for header in df.columns[3:] if header not in columns_to_drop]
-    if 'reflectance' not in df.columns:
-        df['reflectance'] = np.zeros(len(df))
-        print('No reflectance detected, column added with zeros.')
+    if "reflectance" not in df.columns:
+        df["reflectance"] = np.zeros(len(df))
+        print("No reflectance detected, column added with zeros.")
     else:
-        print('Reflectance detected')
+        print("Reflectance detected")
     cols = list(df.columns)
-    if 'reflectance' in cols:
-        cols.insert(3, cols.pop(cols.index('reflectance')))
+    if "reflectance" in cols:
+        cols.insert(3, cols.pop(cols.index("reflectance")))
         df = df[cols]
-    return df, headers, 'reflectance' in df.columns
+    return df, headers, "reflectance" in df.columns
 
-'''
+
+"""
 -------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------------
-'''
+"""
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--point-cloud', '-p', default=[], nargs='+', type=str, help='list of point cloud files')    
-    parser.add_argument('--odir', type=str, default='.', help='output directory')
-    parser.add_argument('--batch_size', default=8, type=int, help="If you get CUDA errors, try lowering this.")
-    parser.add_argument('--num_procs', default=-1, type=int, help="Number of CPU cores you want to use. If you run out of RAM, lower this.")
-    parser.add_argument('--resolution', type=float, default=0.01, help='Resolution to which point cloud is downsampled [m]')
-    parser.add_argument('--grid_size', type=float, nargs='+', default=[2.0, 4.0], help='Grid sizes for voxelization')
-    parser.add_argument('--min_pts', type=int, default=128, help='Minimum number of points in voxel')
-    parser.add_argument('--max_pts', type=int, default=16384, help='Maximum number of points in voxel')
-    parser.add_argument('--model', type=str, default='model.pth', help='path to candidate model')
-    parser.add_argument('--is-wood', default=0.5, type=float, help='a probability above which points within KNN are classified as wood')
-    parser.add_argument('--any-wood', default=1, type=float, help='a probability above which ANY point within KNN is classified as wood')
-    parser.add_argument('--output_fmt', default='ply', help="file type of output")
-    parser.add_argument('--verbose', action='store_true', help="print stuff")
+    parser.add_argument(
+        "--point-cloud",
+        "-p",
+        default=[],
+        nargs="+",
+        type=str,
+        help="list of point cloud files",
+    )
+    parser.add_argument("--odir", type=str, default=".", help="output directory")
+    parser.add_argument(
+        "--batch_size",
+        default=8,
+        type=int,
+        help="If you get CUDA errors, try lowering this.",
+    )
+    parser.add_argument(
+        "--num_procs",
+        default=-1,
+        type=int,
+        help="Number of CPU cores you want to use. If you run out of RAM, lower this.",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=float,
+        default=0.01,
+        help="Resolution to which point cloud is downsampled [m]",
+    )
+    parser.add_argument(
+        "--grid_size",
+        type=float,
+        nargs="+",
+        default=[2.0, 4.0],
+        help="Grid sizes for voxelization",
+    )
+    parser.add_argument(
+        "--min_pts", type=int, default=128, help="Minimum number of points in voxel"
+    )
+    parser.add_argument(
+        "--max_pts", type=int, default=16384, help="Maximum number of points in voxel"
+    )
+    parser.add_argument(
+        "--model", type=str, default="model.pth", help="path to candidate model"
+    )
+    parser.add_argument(
+        "--is-wood",
+        default=0.5,
+        type=float,
+        help="a probability above which points within KNN are classified as wood",
+    )
+    parser.add_argument(
+        "--any-wood",
+        default=1,
+        type=float,
+        help="a probability above which ANY point within KNN is classified as wood",
+    )
+    parser.add_argument("--output_fmt", default="ply", help="file type of output")
+    parser.add_argument(
+        "--gpu",
+        type=str,
+        default="cuda:1",
+        help="GPU device to use (e.g., cuda:0, cuda:1, cuda:2). Default: cuda:1",
+    )
+    parser.add_argument("--verbose", action="store_true", help="print stuff")
 
     args = parser.parse_args()
+
+    # Validate GPU device
+    if args.gpu.startswith("cuda:"):
+        gpu_id = int(args.gpu.split(":")[1])
+        if not torch.cuda.is_available():
+            print(f"Warning: CUDA is not available. Falling back to CPU.")
+            args.gpu = "cpu"
+        elif gpu_id >= torch.cuda.device_count():
+            print(
+                f"Warning: GPU {gpu_id} not available. Available GPUs: 0-{torch.cuda.device_count() - 1}. Using GPU 0."
+            )
+            args.gpu = "cuda:0"
+        else:
+            print(f"Using GPU device: {args.gpu}")
+    elif args.gpu == "cpu":
+        print("Using CPU device")
+    else:
+        print(f"Warning: Invalid GPU device '{args.gpu}'. Using CPU.")
+        args.gpu = "cpu"
 
     # Configure the number of threads based on args.num_procs
     if args.num_procs == -1:
@@ -84,67 +160,75 @@ if __name__ == '__main__':
     set_num_threads(num_threads)
 
     if args.verbose:
-        print('\n---- parameters used ----')
+        print("\n---- parameters used ----")
         for k, v in args.__dict__.items():
-            if k == 'pc': v = '{} points'.format(len(v))
-            if k == 'global_shift': v = v.values
-            print('{:<35}{}'.format(k, v)) 
+            if k == "pc":
+                v = "{} points".format(len(v))
+            if k == "global_shift":
+                v = v.values
+            print("{:<35}{}".format(k, v))
 
     args.wdir = get_path()
-    args.mode = 'predict' if 'predict' in sys.argv[0] else 'train'
+    args.mode = "predict" if "predict" in sys.argv[0] else "train"
     args.reflectance = False
 
-    '''
+    """
     Sanity check---------------------------------------------------------------------------------------------------------
-    '''
-    if args.point_cloud == '':
-        raise Exception('no input specified, please specify --point-cloud')
-    
+    """
+    if args.point_cloud == "":
+        raise Exception("no input specified, please specify --point-cloud")
+
     for point_cloud_file in args.point_cloud:
         if not os.path.isfile(point_cloud_file):
-            raise FileNotFoundError(f'Point cloud file not found: {point_cloud_file}')
-    
-    '''
+            raise FileNotFoundError(f"Point cloud file not found: {point_cloud_file}")
+
+    """
     If voxel file on disc, delete it.
-    '''    
-    
+    """
+
     path = OP.dirname(args.point_cloud[0])
     args.vxfile = OP.join(path, "voxels")
 
-    if os.path.exists(args.vxfile): shutil.rmtree(args.vxfile)
+    if os.path.exists(args.vxfile):
+        shutil.rmtree(args.vxfile)
 
     for point_cloud_file in args.point_cloud:
-
-        '''
+        """
         Handle input and output file paths-----------------------------------------------------------------------------------
-        '''
-        
+        """
+
         path = OP.dirname(point_cloud_file)
         file = OP.splitext(OP.basename(point_cloud_file))[0] + "_ours.ply"
         args.odir = OP.join(path, file)
 
-        '''
+        """
         Preprocess data into voxels------------------------------------------------------------------------------------------
-        '''
+        """
 
-        if args.verbose: print('\n----- Preprocessing started -----')
+        if args.verbose:
+            print("\n----- Preprocessing started -----")
 
         os.makedirs(args.vxfile, exist_ok=True)
-        args.pc, args.headers = load_file(filename=point_cloud_file, additional_headers=True, verbose=False)
+        args.pc, args.headers = load_file(
+            filename=point_cloud_file, additional_headers=True, verbose=False
+        )
         args.pc, args.headers, args.reflectance = preprocess_point_cloud_data(args.pc)
-        
-        print(f'Voxelising to {args.grid_size} grid sizes')
-        preprocess(args)
-        
+
+        print(f"Voxelising to {args.grid_size} grid sizes")
+        preprocess(args, device=args.gpu)
+
         if args.verbose:
-            print(f'peak memory: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6}')
-            print(f'runtime: {(datetime.datetime.now() - start).seconds}')
-        
-        '''
+            print(
+                f"peak memory: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6}"
+            )
+            print(f"runtime: {(datetime.datetime.now() - start).seconds}")
+
+        """
         Run semantic training------------------------------------------------------------------------------------------------
-        '''
-        if args.verbose: print('\n----- Semantic segmenation started -----')
-        
+        """
+        if args.verbose:
+            print("\n----- Semantic segmenation started -----")
+
         SemanticSegmentation(args)
         torch.cuda.empty_cache()
 
@@ -152,5 +236,7 @@ if __name__ == '__main__':
             shutil.rmtree(args.vxfile)
 
         if args.verbose:
-            print(f'peak memory: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6}')
-            print(f'runtime: {(datetime.datetime.now() - start).seconds}')
+            print(
+                f"peak memory: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6}"
+            )
+            print(f"runtime: {(datetime.datetime.now() - start).seconds}")
--- pointstowood/src/preprocessing.py
+++ pointstowood/src/preprocessing.py
@@ -6,14 +6,25 @@ import os
 from tqdm import tqdm
 import torch_scatter
 
+
 class Voxelise:
-    def __init__(self, pos, vxpath, minpoints=512, maxpoints=16384, gridsize=[2.0,4.0], pointspacing=0.01):
+    def __init__(
+        self,
+        pos,
+        vxpath,
+        minpoints=512,
+        maxpoints=16384,
+        gridsize=[2.0, 4.0],
+        pointspacing=0.01,
+        device="cuda:1",
+    ):
         self.pos = pos
         self.vxpath = vxpath
         self.minpoints = minpoints
         self.maxpoints = maxpoints
         self.gridsize = gridsize
         self.pointspacing = min(self.gridsize) / 100.0
+        self.device = device
 
     def quantile_normalize_reflectance(self):
         reflectance_tensor = self.pos[:, 3].view(-1)
@@ -23,12 +34,16 @@ class Voxelise:
         ranks = torch.argsort(indices)
         empirical_quantiles = (ranks.float() + 1) / (len(ranks) + 1)
         empirical_quantiles = torch.clamp(empirical_quantiles, 1e-7, 1 - 1e-7)
-        normalized_reflectance = torch.erfinv(2 * empirical_quantiles - 1) * torch.sqrt(torch.tensor(2.0)).to(reflectance_tensor.device)
+        normalized_reflectance = torch.erfinv(2 * empirical_quantiles - 1) * torch.sqrt(
+            torch.tensor(2.0)
+        ).to(reflectance_tensor.device)
         min_val = normalized_reflectance.min()
         max_val = normalized_reflectance.max()
-        scaled_reflectance = 2 * (normalized_reflectance - min_val) / (max_val - min_val) - 1
+        scaled_reflectance = (
+            2 * (normalized_reflectance - min_val) / (max_val - min_val) - 1
+        )
         return scaled_reflectance
-    
+
     def downsample(self):
         voxelised = voxel_grid(self.pos, self.pointspacing)
         _, idx = consecutive_cluster(voxelised)
@@ -36,12 +51,20 @@ class Voxelise:
 
     def gpu_ground(self):
         self.pos = self.pos.contiguous()
-        x, y, z = self.pos[:, 0].contiguous(), self.pos[:, 1].contiguous(), self.pos[:, 2].contiguous()
+        x, y, z = (
+            self.pos[:, 0].contiguous(),
+            self.pos[:, 1].contiguous(),
+            self.pos[:, 2].contiguous(),
+        )
         grid_resolution = 5.0
         x_min, x_max = torch.min(x), torch.max(x)
         y_min, y_max = torch.min(y), torch.max(y)
-        x_bins = torch.arange(x_min, x_max + grid_resolution, grid_resolution, device='cuda')
-        y_bins = torch.arange(y_min, y_max + grid_resolution, grid_resolution, device='cuda')
+        x_bins = torch.arange(
+            x_min, x_max + grid_resolution, grid_resolution, device=self.device
+        )
+        y_bins = torch.arange(
+            y_min, y_max + grid_resolution, grid_resolution, device=self.device
+        )
         x_indices = torch.bucketize(x, x_bins)
         y_indices = torch.bucketize(y, y_bins)
         grid_indices = x_indices * len(y_bins) + y_indices
@@ -51,7 +74,7 @@ class Voxelise:
         normalized_z = z - min_z_per_point  # Preserve units in meters
         self.pos = torch.cat((self.pos, normalized_z.view(-1, 1)), dim=1)
         return self.pos
-        
+
     def grid(self):
         indices_list = []
         for size in self.gridsize:
@@ -60,9 +83,9 @@ class Voxelise:
                 voxel = (voxelised == vx).nonzero(as_tuple=True)[0]
                 if voxel.size(0) < self.minpoints:
                     continue
-                indices_list.append(voxel.to('cpu'))
+                indices_list.append(voxel.to("cpu"))
         return indices_list
-    
+
     def reflectance2intensity(self):
         reflectance_tensor = self.pos[:, 3]
         if torch.isnan(reflectance_tensor).any():
@@ -70,65 +93,84 @@ class Voxelise:
         min_reflectance = reflectance_tensor.min()
         max_reflectance = reflectance_tensor.max()
         if max_reflectance == min_reflectance:
-            raise ValueError("max_reflectance and min_reflectance are equal, leading to division by zero.")
-        scaled_reflectance = (reflectance_tensor - min_reflectance) / (max_reflectance - min_reflectance)
+            raise ValueError(
+                "max_reflectance and min_reflectance are equal, leading to division by zero."
+            )
+        scaled_reflectance = (reflectance_tensor - min_reflectance) / (
+            max_reflectance - min_reflectance
+        )
         intensity = scaled_reflectance * 65535
         self.pos[:, 3] = scaled_reflectance
         return self.pos
-    
+
     def write_voxels(self):
         # Check if 'n_z' is in the DataFrame before converting to tensor
-        if 'n_z' not in self.pos.columns:
-            print('Height Normalising Point Cloud')
-            self.pos = torch.tensor(self.pos.values, dtype=torch.float).to(device='cuda')
+        if "n_z" not in self.pos.columns:
+            print("Height Normalising Point Cloud")
+            self.pos = torch.tensor(self.pos.values, dtype=torch.float).to(
+                device=self.device
+            )
             self.pos = self.gpu_ground()
         else:
-            self.pos = torch.tensor(self.pos.values, dtype=torch.float).to(device='cuda')
-                
+            self.pos = torch.tensor(self.pos.values, dtype=torch.float).to(
+                device=self.device
+            )
+
         reflectance_not_zero = not torch.all(self.pos[:, 3] == 0)
-        
-        #self.reflectance2intensity()
+
+        # self.reflectance2intensity()
 
         if reflectance_not_zero:
             self.pos[:, 3] = self.quantile_normalize_reflectance()
 
         voxels = self.grid()
 
-        
         if reflectance_not_zero:
             weight = self.pos[:, 3] - self.pos[:, 3].min()
             mask = ~(torch.isnan(weight) | torch.isinf(weight))
             self.pos, weight = self.pos[mask], weight[mask]
             if weight.sum() == 0:
-                raise ValueError("All weights are invalid. Check the reflectance values.")
+                raise ValueError(
+                    "All weights are invalid. Check the reflectance values."
+                )
             weight = weight + 1e-8
-            weight = weight.detach().to('cpu')
+            weight = weight.detach().to("cpu")
         else:
             weight = None
 
-        self.pos = self.pos.detach().clone().to('cpu')
-        file_counter = len(glob.glob(os.path.join(self.vxpath, 'voxel_*.pt')))
+        self.pos = self.pos.detach().clone().to("cpu")
+        file_counter = len(glob.glob(os.path.join(self.vxpath, "voxel_*.pt")))
 
-        for _, voxel_indices in enumerate(tqdm(voxels, desc='Writing voxels')):
+        for _, voxel_indices in enumerate(tqdm(voxels, desc="Writing voxels")):
             if voxel_indices.size(0) == 0:
-                continue  
+                continue
 
             if voxel_indices.size(0) > self.maxpoints:
                 if reflectance_not_zero:
-                    voxel_indices = voxel_indices[torch.multinomial(weight[voxel_indices], self.maxpoints)]
+                    voxel_indices = voxel_indices[
+                        torch.multinomial(weight[voxel_indices], self.maxpoints)
+                    ]
                 else:
-                    voxel_indices = voxel_indices[torch.randint(0, voxel_indices.size(0), (self.maxpoints,))]
-            
+                    voxel_indices = voxel_indices[
+                        torch.randint(0, voxel_indices.size(0), (self.maxpoints,))
+                    ]
+
             voxel = self.pos[voxel_indices]
             voxel = voxel[~torch.isnan(voxel).any(dim=1)]
-            
-            torch.save(voxel, os.path.join(self.vxpath, f'voxel_{file_counter}.pt'))
+
+            torch.save(voxel, os.path.join(self.vxpath, f"voxel_{file_counter}.pt"))
             file_counter += 1
         return self.pos[:, -1]
 
-def preprocess(args):
-    n_z = Voxelise(args.pc, vxpath=args.vxfile, minpoints=args.min_pts, maxpoints=args.max_pts, pointspacing=args.resolution, gridsize = args.grid_size).write_voxels()
-    args.pc['n_z'] = n_z.detach().numpy()
 
-
-
+def preprocess(args, device="cuda:1"):
+    n_z = Voxelise(
+        args.pc,
+        vxpath=args.vxfile,
+        minpoints=args.min_pts,
+        maxpoints=args.max_pts,
+        pointspacing=args.resolution,
+        gridsize=args.grid_size,
+        device=device,
+    ).write_voxels()
+    args.pc["n_z"] = n_z.detach().numpy()
